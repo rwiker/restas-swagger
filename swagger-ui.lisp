e06954d42e6ca8ac4f28f3e4909f7b7708cb25e0
Raymond Wiker <rayw@equinor.com>
2020-08-20 11:10:42 +0200

    Added documentation stuff.

--- swagger-ui.lisp
+++ swagger-ui.lisp
@@ -3,8 +3,11 @@
 (defparameter *swagger-ui-module* nil)
 
 (restas:define-route root ("" :method :get :content-type "text/html")
+  "Interactive Swagger UI."
   (merge-pathnames #p"./static/index.html"
                    (asdf/system:system-source-directory :restas-swagger)))
 
 (restas:define-route swagger/json ("swagger.json" :method :get :content-type "application/json")
-    (get-swagger-definition/json *swagger-ui-module*))
+  (:example-uri)
+  "Get swagger definition for current module (by *swagger-ui-module*)."
+  (get-swagger-definition/json *swagger-ui-module*))
